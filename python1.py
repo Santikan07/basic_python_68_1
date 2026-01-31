d5da87d1a8e9df9333b2cc7505ec2e246454e75c
Santikan07 <wsgegdg@gmail.com>
2026-01-31 13:21:58 +0700

    python variables

---
+++ python1.py
@@ -0,0 +1,15 @@
+print("hello, world!")
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("10")
+z = float("5.0")
+print(x, y, z)
+
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
