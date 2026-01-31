401ed6b45f275fdb751a514cf033bf401fcc46e3
Santikan07 <wsgegdg@gmail.com>
2026-01-31 14:11:34 +0700

    Collection of python

---
+++ python3.py
@@ -0,0 +1,54 @@
+"""
+#
+# Part: Collection of python
+# Lists, Tuples, Sets, Dictionaries
+#
+"""
+
+"""
+#
+# list
+# 
+#
+"""
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# tuple
+# 
+"""
+this_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(this_tuple)
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+"""
+#
+# sets
+# 
+"""
+this_set = {"apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"}
+print(this_set)
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# dictionraries
+# 
+"""
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(this_dict)
+print(this_dict["model"])
+print(type(this_dict))
+print(len(this_dict))
